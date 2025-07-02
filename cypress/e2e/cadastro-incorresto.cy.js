85fe035e2e9cbf79cc5d247562af6704f463faaf
Felipe Colins <felipe_colins@hotmail.com>
2025-07-02 13:11:03 +0100

    Teste em massa

--- cypress/e2e/cadastro-incorresto.cy.js
+++ cypress/e2e/cadastro-incorresto.cy.js
@@ -1,11 +1,12 @@
-describe('Página de cadastro', () => {
-  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.get('[data-test="register-button"]').click();
-    cy.get('[data-test="input-name"]').type('Ana de Jesus');
-    cy.get('[data-test="input-email"]').type('ana@email.com');
-    cy.get('[data-test="input-password"]').type('Senha123');
-    cy.get('[data-test="input-confirm-password"]').type('Senha123');
-    cy.get('[data-test="submit-button"]').click();
-  })
-})
+describe('Página de login', ()=> {
+    beforeEach(()=> {
+        cy.visit('https://adopet-frontend-cypress.vercel.app');
+        cy.get('[data-test="login-button"]').click();
+        })
+                   
+    it('Verifica mensagem de falha no login', () => {
+        cy.get('[data-test="submit-button"]').click()
+        cy.contains('É necessário informar um endereço de email').should('be.visible')
+        cy.contains('Insira sua senha').should('be.visible')
+    })
+    })
